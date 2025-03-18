e20c33ac80611b5aac29b6fd8081be18f7eb2984
Gustavo Ferreira <gustavoacacio22@gmail.com>
2025-03-18 20:27:38 -0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? '/exelentic/' : '',
+  basePath: isProd ? '/exelentic' : '',
+  output: 'export'
 };
 
 export default nextConfig;
